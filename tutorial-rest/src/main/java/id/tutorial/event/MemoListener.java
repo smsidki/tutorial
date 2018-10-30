4559dee79a89ba91fbc15f960963202bed16387e
smsidki <sichy_helsinky@yahoo.com>
2018-10-30 22:25:20 +0700

    Spring Event
    - Listen to memo event

---
+++ tutorial-rest/src/main/java/id/tutorial/event/MemoListener.java
@@ -0,0 +1,23 @@
+package id.tutorial.event;
+
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class MemoListener {
+	
+	private static final DateTimeFormatter DTF = DateTimeFormatter.ofPattern("hh:mm a");
+	
+	@EventListener
+	public void showMemo(Memo memo) {
+		log.info("Hi {}, please meet me at {} on {}.", memo.getPerson(), DTF.format(memo.getTime()), memo.getLocation());
+		log.info("We will discuss about {}.", memo.getTopic());
+		log.info("Thank you.");
+	}
+
+}
