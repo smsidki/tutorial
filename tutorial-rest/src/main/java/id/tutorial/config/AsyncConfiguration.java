72c9af2acf981e27ab823a3cdfa378af0a311001
smsidki <sichy_helsinky@yahoo.com>
2018-11-03 17:44:59 +0700

    Spring Async
    -  Async exception handler

--- tutorial-rest/src/main/java/id/tutorial/config/AsyncConfiguration.java
+++ tutorial-rest/src/main/java/id/tutorial/config/AsyncConfiguration.java
@@ -1,32 +1,41 @@
 package id.tutorial.config;
 
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ConcurrentTaskExecutor;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+import id.tutorial.config.error.AsyncErrorHandler;
+
 @EnableAsync
 @Configuration
-public class AsyncConfiguration {
+public class AsyncConfiguration implements AsyncConfigurer {
 	
+	@Override
+	public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+		return new AsyncErrorHandler();
+	}
+
 	@Bean
 	public TaskExecutor taskExecutor() {
 		return new SimpleAsyncTaskExecutor("defaultTaskExecutor-");
 	}
-	
+
 	@Bean
 	public TaskExecutor poolTaskExecutor() {
 		ThreadPoolTaskExecutor poolTaskExecutor = new ThreadPoolTaskExecutor();
 		poolTaskExecutor.initialize();
 		return poolTaskExecutor;
 	}
-	
+
 	@Bean
 	public TaskExecutor concurrentTaskExecutor() {
 		return new ConcurrentTaskExecutor();
 	}
-
+	
 }
