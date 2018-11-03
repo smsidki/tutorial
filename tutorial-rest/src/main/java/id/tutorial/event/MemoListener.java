9562d2f2c412f16a2b0222d3fb4c7e591272693f
smsidki <sichy_helsinky@yahoo.com>
2018-11-03 12:30:10 +0700

    Spring Event
    - With @Async

--- tutorial-rest/src/main/java/id/tutorial/event/MemoListener.java
+++ tutorial-rest/src/main/java/id/tutorial/event/MemoListener.java
@@ -3,6 +3,7 @@ package id.tutorial.event;
 import java.time.format.DateTimeFormatter;
 
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
@@ -13,8 +14,11 @@ public class MemoListener {
 	
 	private static final DateTimeFormatter DTF = DateTimeFormatter.ofPattern("hh:mm a");
 	
+	@Async
 	@EventListener
-	public void showMemo(Memo memo) {
+	public void showMemo(Memo memo) throws InterruptedException {
+		Thread.sleep(3000L);
+		
 		log.info("Hi {}, please meet me at {} on {}.", memo.getPerson(), DTF.format(memo.getTime()), memo.getLocation());
 		log.info("We will discuss about {}.", memo.getTopic());
 		log.info("Thank you.");
