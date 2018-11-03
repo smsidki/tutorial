ecd0103942fd6f25ba922b1b753f32dd36b1ed0e
smsidki <sichy_helsinky@yahoo.com>
2018-11-03 12:25:37 +0700

    Spring Async
    - Task executor

--- tutorial-rest/src/main/java/id/tutorial/config/AsyncConfiguration.java
+++ tutorial-rest/src/main/java/id/tutorial/config/AsyncConfiguration.java
@@ -1,10 +1,32 @@
 package id.tutorial.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ConcurrentTaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @EnableAsync
 @Configuration
 public class AsyncConfiguration {
+	
+	@Bean
+	public TaskExecutor taskExecutor() {
+		return new SimpleAsyncTaskExecutor("defaultTaskExecutor-");
+	}
+	
+	@Bean
+	public TaskExecutor poolTaskExecutor() {
+		ThreadPoolTaskExecutor poolTaskExecutor = new ThreadPoolTaskExecutor();
+		poolTaskExecutor.initialize();
+		return poolTaskExecutor;
+	}
+	
+	@Bean
+	public TaskExecutor concurrentTaskExecutor() {
+		return new ConcurrentTaskExecutor();
+	}
 
 }
