5a388790dd2fcf4567f3459984add0cfc4d3c6cd
smsidki <sichy_helsinky@yahoo.com>
2018-11-03 11:29:59 +0700

    Spring Async
    - Simple sample

---
+++ tutorial-rest/src/main/java/id/tutorial/service/GreetingService.java
@@ -0,0 +1,23 @@
+package id.tutorial.service;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class GreetingService {
+	
+	@Async
+	public void doSomeHeavyMethod() {
+		try {
+			Thread.sleep(3000L);
+		} catch (InterruptedException e) {
+			log.error(e.getMessage(), e);
+			Thread.currentThread().interrupt();
+		}
+		log.info("Execution done");
+	}
+
+}
