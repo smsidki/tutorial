004bc7a880d8f93c88c8f038f8d75b2ebd7a3f57
smsidki <sichy_helsinky@yahoo.com>
2018-11-03 17:12:00 +0700

    Spring Async
    -  @Async with return type

--- tutorial-rest/src/main/java/id/tutorial/service/GreetingService.java
+++ tutorial-rest/src/main/java/id/tutorial/service/GreetingService.java
@@ -1,6 +1,9 @@
 package id.tutorial.service;
 
+import java.util.concurrent.Future;
+
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Service;
 
 import lombok.extern.slf4j.Slf4j;
@@ -8,7 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Service
 public class GreetingService {
-	
+
 	@Async
 	public void doSomeHeavyMethod() {
 		log.info(Thread.currentThread().getName());
@@ -19,6 +22,17 @@ public class GreetingService {
 			Thread.currentThread().interrupt();
 		}
 		log.info("Execution done");
+	}
+
+	@Async
+	public Future<String> generateGreetingMessage() {
+		try {
+			Thread.sleep(1000L);
+		} catch (InterruptedException e) {
+			log.error(e.getMessage(), e);
+			Thread.currentThread().interrupt();
+		}
+		return new AsyncResult<>("Hello World");
 	}
 
 }
